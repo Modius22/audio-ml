eeee30ab4fcaaa5781de93b3605cc4695245c0f0
Modius22 <christian.piazzi@gmx.de>
2022-06-09 20:01:13 +0200

    modify optuna call to mutli model eval

--- src/libs/models.py
+++ src/libs/models.py
@@ -1,20 +1,18 @@
 from sklearn import ensemble, linear_model, model_selection, tree
-from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
-from xgboost import XGBClassifier
 
 from libs import utils
 
 
 # Step 1. Define an objective function to be maximized.
-def objective(trial, models, smote):
-    X, y, X_t, y_t = utils.get_dataset(notebook=True, smote=smote)
+def objective(trial, models):
+    X, y, X_t, y_t = utils.get_dataset(notebook=True)
+
+    X.drop(columns=["Unnamed: 0"], inplace=True)
 
     # classifier_name = trial.suggest_categorical("classifier", ["LogReg","DeTree", "RandomForest"])
     classifier_name = trial.suggest_categorical("classifier", models)
-
-    # Step 2. Setup values for the hyperparameters:
     if classifier_name == "LogReg":
         logreg_c = trial.suggest_float("logreg_c", 1e-10, 1e10, log=True)
         logreg_solver = trial.suggest_categorical("logreg_solver", ["newton-cg", "lbfgs", "liblinear"])
@@ -31,14 +29,13 @@ def objective(trial, models, smote):
             min_samples_leaf=dt_min_samples_leaf,
             min_weight_fraction_leaf=dt_min_weight_fraction_leaf,
         )
+
     elif classifier_name == "SVC":
         svc_C = trial.suggest_float("svc_C", 0.1, 20)
         svc_gamma = trial.suggest_float("svc_gamma", 0.00001, 10)
         svc_kernel = trial.suggest_categorical("svc_kernel", ["rbf", "sigmoid"])
         classifier_obj = SVC(C=svc_C, gamma=svc_gamma, kernel=svc_kernel)
-    elif classifier_name == "GaussianNB":
-        gnb_var_smoothing = trial.suggest_float("gnb_var_smoothing", 1e-10, 1e10, log=True)
-        classifier_obj = GaussianNB(var_smoothing=gnb_var_smoothing)
+
     elif classifier_name == "MLP":
         mlp_activation = trial.suggest_categorical("mlp_activation", ["tanh", "relu"])
         mlp_solver = trial.suggest_categorical(
@@ -61,34 +58,27 @@ def objective(trial, models, smote):
             learning_rate=mlp_learning_rate,
         )
 
-    elif classifier_name == "XGBClassifier":
-        XGBClassifier_n_estimators = trial.suggest_int("XGBClassifier_n_estimators", 1, 1000)
-        XGBClassifier_max_depth = trial.suggest_int("XGBClassifier_max_depth", 1, 200)
-        XGBClassifier_learning_rate = trial.suggest_float("XGBClassifier_learning_rate", 0.01, 20)
-        classifier_obj = XGBClassifier(
-            n_estimators=XGBClassifier_n_estimators,
-            max_depth=XGBClassifier_max_depth,
-            learning_rate=XGBClassifier_learning_rate,
-        )
-    else:
+    elif classifier_name == "RandomForest":
         rf_n_estimators = trial.suggest_int("rf_n_estimators", 10, 1000)
         rf_max_depth = trial.suggest_int("rf_max_depth", 10, 100)
-        rf_max_features = trial.suggest_int("rf_max_features", 1, 10)
+        # rf_max_features = trial.suggest_int("rf_max_features", 1, 10)
         rf_min_samples_leaf = trial.suggest_int("rf_min_samples_leaf", 1, 10)
         rf_min_samples_split = trial.suggest_int("rf_min_samples_split", 2, 12)
         rf_random_state = trial.suggest_int("random_state", 0, 42)
-        rf_weights = trial.suggest_int("rf_weights", 0, 400)
+        # rf_weights = trial.suggest_int("rf_weights", 0, 400)
         classifier_obj = ensemble.RandomForestClassifier(
             n_estimators=rf_n_estimators,
             max_depth=rf_max_depth,
-            max_features=rf_max_features,
+            #  max_features=rf_max_features,
             min_samples_leaf=rf_min_samples_leaf,
             min_samples_split=rf_min_samples_split,
             random_state=rf_random_state,
-            class_weight={0: rf_weights, 1: 1},
+            #   class_weight={0: rf_weights, 1: 1},
         )
+    else:
+        pass
 
     # Step 3: Scoring method:
-    score = model_selection.cross_val_score(classifier_obj, X, y, n_jobs=-1, cv=10, scoring="f1")
-    f1 = score.mean()
-    return f1
+    score = model_selection.cross_val_score(classifier_obj, X, y, n_jobs=-1, cv=10)
+    accuracy = score.mean()
+    return accuracy
--- src/libs/utils.py
+++ src/libs/utils.py
@@ -1,13 +1,11 @@
 # from typing import bool
 
 import pandas as pd
-from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
 
 
-def get_dataset(
-    notebook: bool = False, smote: bool = False
-) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
+def get_dataset(notebook: bool = False) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
+
     if not notebook:
         train_dataset = pd.read_csv("data/data.csv")
     else:
@@ -16,8 +14,5 @@ def get_dataset(
     X_train, X_test, y_train, y_test = train_test_split(
         train_dataset.drop(columns=["type"]), train_dataset["type"], test_size=0.33, random_state=42
     )
-
-    if smote:
-        X_train, y_train = SMOTE(random_state=42).fit_resample(X_train, y_train)
 
     return X_train, y_train, X_test, y_test
